6ae7b2a13c8b9737662358d5b08456987a70b9e8
Your NameJanAlSaifi <jan.al.saifi@googlemail.com>
2024-11-25 14:52:59 +0100

    added colorForm.jsx and colorInput.jsx, updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [colors, setColor] = useState(initialColors);
+
+  function handleSubmitColorForm(newColor) {
+    console.log("New Color Submitted:", newColor);
+
+    const formattedColor = {
+      id: Date.now(),
+      role: newColor["role-name"],
+      hex: newColor["hex-color"],
+      contrastText: newColor["contrast-color"],
+    };
+    console.log("Formatted Color:", formattedColor);
+
+    setColor([...colors, formattedColor]);
+  }
+  console.log(colors);
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
+      <ColorForm onSubmitColor={handleSubmitColorForm} />
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
---
+++ src/Components/ColorForm/ColorForm.jsx
@@ -0,0 +1,42 @@
+import "./ColorForm.css";
+import ColorInput from "../ColorInput/ColorInput";
+
+export default function ColorForm({
+  onSubmitColor,
+  initialData = { role: "primary", hex: "#123456", contrastText: "#ffffff" },
+}) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onSubmitColor(data);
+    console.log(data);
+  }
+
+  return (
+    <div>
+      <form className="Form-Card" onSubmit={handleSubmit}>
+        <label htmlFor="role-name">Role</label>
+        <input
+          type="text"
+          id="role-name"
+          name="role-name"
+          defaultValue={initialData.role}
+        ></input>
+        <label htmlFor="hex-color">Hex</label>
+        <ColorInput
+          id="hex-color"
+          name="hex-color"
+          defaultValue={initialData.hex}
+        />
+        <label htmlFor="contrast-color">Contrast Text</label>
+        <ColorInput
+          id="contrast-color"
+          name="contrast-color"
+          defaultValue={initialData.contrastText}
+        />
+        <button type="submit">ADD COLOR</button>
+      </form>
+    </div>
+  );
+}
